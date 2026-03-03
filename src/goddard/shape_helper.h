db1f66301ed821c41a51b6382e46d883c8351e71
ExcellentGamer <excellentgamer417@gmail.com>
2026-03-03 05:36:04 -0500

    Fixed some animation issues

--- src/goddard/shape_helper.h
+++ src/goddard/shape_helper.h
@@ -31,6 +31,7 @@ bool gd_dynos_goddard_has_skin_weights(void);
 u32 gd_dynos_goddard_get_skin_joint_count(void);
 bool gd_dynos_goddard_get_skin_joint_data(u32 index, u32 *out_joint_id, u32 *out_weight_count);
 bool gd_dynos_goddard_get_skin_weight(u32 joint_index, u32 weight_index, u16 *out_vtx_idx, f32 *out_weight);
+void gd_dynos_apply_gdb2_skin_weights(void);
 
 // see bad_declarations.h
 #ifndef GD_USE_BAD_DECLARATIONS
