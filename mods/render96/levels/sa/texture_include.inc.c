471e3dd851730a4d7aeccf6ba76a09c8a7c2cf11
ExcellentGamer <excellentgamer417@gmail.com>
2026-03-04 01:16:06 -0500

    SA update and mod size increased

--- mods/render96/levels/sa/texture_include.inc.c
+++ mods/render96/levels/sa/texture_include.inc.c
@@ -1,20 +1,20 @@
-Texture sa_dl_inside_castle_textures_0B000_rgba16_rgba16[] = {
-	#include "levels/sa/inside_castle_textures.0B000.rgba16.inc.c"
+Texture sa_dl_tiles_rgba16[] = {
+	#include "levels/sa/tiles.rgba16.inc.c"
 };
 
-Texture sa_dl_sa_reflection_gold_rgba16[] = {
-	#include "levels/sa/sa_reflection_gold.rgba16.inc.c"
+Texture sa_dl_gold_reflection_rgba16[] = {
+	#include "levels/sa/gold_reflection.rgba16.inc.c"
 };
 
 Texture sa_dl_pillar_rgba16[] = {
 	#include "levels/sa/pillar.rgba16.inc.c"
 };
 
-Texture sa_dl_inside_castle_textures_03000_rgba16_rgba16[] = {
-	#include "levels/sa/inside_castle_textures.03000.rgba16.inc.c"
+Texture sa_dl_bricks_rgba16[] = {
+	#include "levels/sa/bricks.rgba16.inc.c"
 };
 
-Texture sa_dl_inside_castle_textures_07000_rgba16_rgba16[] = {
-	#include "levels/sa/inside_castle_textures.07000.rgba16.inc.c"
+Texture sa_dl_window_bars_rgba16[] = {
+	#include "levels/sa/window_bars.rgba16.inc.c"
 };
 
