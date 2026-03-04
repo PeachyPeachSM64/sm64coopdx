ecf288bece9dfafbb2b6aba7d5d516dcf1d96ffe
ExcellentGamer <excellentgamer417@gmail.com>
2026-03-04 08:03:00 -0500

    Fourth floor slot added (we will probably have to replace the base level)

---
+++ levels/fourth_floor/script.c
@@ -0,0 +1,42 @@
+#include <ultra64.h>
+#include "sm64.h"
+#include "behavior_data.h"
+#include "model_ids.h"
+#include "seq_ids.h"
+#include "dialog_ids.h"
+#include "segment_symbols.h"
+#include "level_commands.h"
+
+#include "game/level_update.h"
+
+#include "levels/scripts.h"
+
+
+#include "make_const_nonconst.h"
+#include "levels/fourth_floor/header.h"
+
+const LevelScript level_fourth_floor_entry[] = {
+	INIT_LEVEL(),
+	LOAD_MIO0(0x7, _fourth_floor_segment_7SegmentRomStart, _fourth_floor_segment_7SegmentRomEnd),
+	ALLOC_LEVEL_POOL(),
+	MARIO(MODEL_MARIO, 0x00000001, bhvMario),
+
+	AREA(1, fourth_floor_area_1),
+		OBJECT(MODEL_NONE, 376, -110, -533, 0, 0, 0, 0x000A0000, bhvSpinAirborneWarp),
+		WARP_NODE(/*id*/ 0x0A, /*destLevel*/ LEVEL_CASTLE, /*destArea*/ 0x01, /*destNode*/ 0x1F, /*flags*/ WARP_NO_CHECKPOINT),
+		WARP_NODE(/*id*/ 0xF0, /*destLevel*/ LEVEL_CASTLE, /*destArea*/ 0x01, /*destNode*/ 0x1F, /*flags*/ WARP_NO_CHECKPOINT),
+		WARP_NODE(/*id*/ 0xF1, /*destLevel*/ LEVEL_CASTLE, /*destArea*/ 0x01, /*destNode*/ 0x1F, /*flags*/ WARP_NO_CHECKPOINT),
+		TERRAIN(fourth_floor_area_1_collision),
+		MACRO_OBJECTS(fourth_floor_area_1_Area_1_macro_objs),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_SOUND_PLAYER),
+		TERRAIN_TYPE(TERRAIN_STONE),
+	END_AREA(),
+
+	FREE_LEVEL_POOL(),
+	MARIO_POS(0x01, 0, 373, -120, 450),
+	CALL(0, lvl_init_or_update),
+	CALL_LOOP(1, lvl_init_or_update),
+	CLEAR_LEVEL(),
+	SLEEP_BEFORE_EXIT(1),
+	EXIT(),
+};
