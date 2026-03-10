1c96accd2fd769ea804a05afdb1dff816ec9bbfa
ExcellentGamer <excellentgamer417@gmail.com>
2026-03-10 05:30:04 -0400

    More language stuff (I used AI for translation so take it with a grain of salt)

--- src/pc/djui/djui_panel_qol_general.c
+++ src/pc/djui/djui_panel_qol_general.c
@@ -5,12 +5,16 @@
 #include "pc/configfile.h"
 
 void djui_panel_qol_general_create(struct DjuiBase* caller) {
-    struct DjuiThreePanel* panel = djui_panel_menu_create("QoL - General", false);
+    struct DjuiThreePanel* panel = djui_panel_menu_create(DLANG(QOL, QOL_GENERAL_TITLE), false);
     struct DjuiBase* body = djui_three_panel_get_body(panel);
     {
-        char* stayInLevelChoices[3] = { "Disabled", "Enabled (Normal)", "Enabled (Non-stop)" };
-        djui_selectionbox_create(body, "Stay in level after star", stayInLevelChoices, 3, &configStayInLevelAfterStar, NULL);
-        djui_checkbox_create(body, "Better Goddard material handling", &configQolBetterGoddardMaterial, NULL);
+        char* stayInLevelChoices[3] = {
+            (char*)DLANG(QOL, CHOICE_DISABLED),
+            (char*)DLANG(QOL, CHOICE_ENABLED_NORMAL),
+            (char*)DLANG(QOL, CHOICE_ENABLED_NONSTOP),
+        };
+        djui_selectionbox_create(body, DLANG(QOL, STAY_IN_LEVEL_AFTER_STAR), stayInLevelChoices, 3, &configStayInLevelAfterStar, NULL);
+        djui_checkbox_create(body, DLANG(QOL, BETTER_GODDARD_MATERIAL_HANDLING), &configQolBetterGoddardMaterial, NULL);
 
         djui_button_create(body, DLANG(MENU, BACK), DJUI_BUTTON_STYLE_BACK, djui_panel_menu_back);
     }
