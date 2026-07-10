77564d81ef95ebfbcecc2e3a63f467e624da1439
PeachyPeachSM64 <72323920+PeachyPeachSM64@users.noreply.github.com>
2026-07-10 21:34:37 +0200

    fix FPS text color

--- src/pc/djui/djui_fps_display.c
+++ src/pc/djui/djui_fps_display.c
@@ -45,6 +45,7 @@ void djui_fps_display_create(void) {
         djui_base_set_size_type(&text->base, DJUI_SVT_RELATIVE, DJUI_SVT_ABSOLUTE);
         djui_base_set_size(&text->base, 1.0f, text->fontScale * 2);
         djui_base_set_location(&text->base, 0, -text->fontScale / 3.0f);
+        djui_base_set_color(&text->base, 220, 220, 220, 255);
 
         fpsDisplay->text = text;
     }
